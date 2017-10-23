553120dd1ddf1c47e0d7374e8f802a57dac6c734
Fabiano Brito <fabianopbrito@gmail.com>
2017-10-23 12:11:36 +0200

    Fiddling with configs to build on heroku.

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../dist')));
+app.use(express.static(path.resolve(__dirname, 'dist')));
 
 app.get('*', (request, response) => {
-  response.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
+  response.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
