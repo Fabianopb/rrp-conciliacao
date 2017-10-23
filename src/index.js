77ecf72f7953d5bf20f0dd4ecd6872649fbfcc79
Fabiano Brito <fabianopbrito@gmail.com>
2017-10-23 19:36:48 +0200

    Configures angular material and creates a simple navbar.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import angular from 'angular';
 import {HomeContainer} from './app/containers/Home';
 
 import 'angular-ui-router';
+import 'angular-material';
+import 'angular-messages';
 
 import routesConfig from './routes';
 
+import 'angular-material/angular-material.css';
 import './app/styles/index.scss';
 
 export const app = 'app';
 
 angular
-  .module(app, ['ui.router'])
+  .module(app, ['ui.router', 'ngMaterial', 'ngMessages'])
   .config(routesConfig)
   .component('home', HomeContainer);
