f3f28b0d7c33ebae2aff3eafe76f33e05f1b0e6b
Fabiano Brito <fabianopbrito@gmail.com>
2017-10-27 18:36:33 +0200

    Creates contact information and form.

--- src/app/containers/Contact.js
+++ src/app/containers/Contact.js
@@ -7,7 +7,19 @@ class ContactController {
     this.init();
   }
 
+  submitContactForm() {
+    if (this.contactForm.$valid) {
+      this.log.log(this.contactFormData);
+      this.log.log('Good! Need to send/submit somewhere...');
+    } else {
+      this.log.log('The form is invalid!');
+    }
+  }
+
   init() {
+    this.contactFormData = {
+      name: '', email: '', phone: '', message: ''
+    };
     this.log.log('Contact initialized');
   }
 }
