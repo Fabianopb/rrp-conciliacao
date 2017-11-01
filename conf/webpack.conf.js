31eaafccabf204824ed78a3108554c07b755e71f
Fabiano Brito <fabianopbrito@gmail.com>
2017-11-01 21:20:23 +0100

    Adds test background hero image.

--- conf/webpack.conf.js
+++ conf/webpack.conf.js
@@ -31,7 +31,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        test: /\.(png|jpg|woff|woff2|eot|ttf|svg)$/,
         loader: 'url-loader?limit=100000'
       },
       {
