abb2f11190c4ea1d16a43f43a38124de5d3d058b
Fabiano Brito <fabianopbrito@gmail.com>
2017-11-08 20:02:31 +0100

    Creates methods for showing and hiding FAQ content.

--- src/app/containers/Operation.js
+++ src/app/containers/Operation.js
@@ -3,10 +3,24 @@ import operationStrings from '../../assets/strings/operation.json';
 
 class OperationController {
   /** @ngInject */
-  constructor() {
+  constructor($log) {
+    this.log = $log;
     this.fourSteps = operationStrings.fourSteps;
     this.validity = operationStrings.validity;
     this.faq = operationStrings.faq;
+    this.init();
+  }
+
+  toggle(index) {
+    this.faqItemVisibility[index] = !this.faqItemVisibility[index];
+  }
+
+  shouldRender(index) {
+    return this.faqItemVisibility[index];
+  }
+
+  init() {
+    this.faqItemVisibility = this.faq.content.map(() => false);
   }
 
 }
