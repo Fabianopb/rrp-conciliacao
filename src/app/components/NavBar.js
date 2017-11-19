e7f5f943dfc31b709dbd824bef9a460a8e930f57
Fabiano Brito <fabianopbrito@gmail.com>
2017-11-19 19:45:59 +0200

    Creates side navigation for mobile users.

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -4,9 +4,10 @@ import sharedStrings from '../../assets/strings/shared.json';
 
 class NavBarController {
   /** @ngInject */
-  constructor($log, $state) {
+  constructor($log, $state, $mdSidenav) {
     this.log = $log;
     this.state = $state;
+    this.mdSidenav = $mdSidenav;
     this.routes = routesStrings.routes;
     this.contactPhone = sharedStrings.contactPhone;
     this.contactEmail = sharedStrings.contactEmail;
@@ -14,10 +15,16 @@ class NavBarController {
 
   goto(path) {
     if (this.state.current.name !== path) {
+      if (this.mdSidenav('left').isOpen()) {
+        this.toggleSideNav();
+      }
       this.state.go(path);
     }
   }
 
+  toggleSideNav() {
+    this.mdSidenav('left').toggle();
+  }
 }
 
 export const NavBarComponent = {
