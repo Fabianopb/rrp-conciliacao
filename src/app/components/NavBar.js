ee7633a814e2c1400a79e6cab66cdb356beb6939
Fabiano Brito <fabianopbrito@gmail.com>
2017-12-04 21:21:34 +0200

    Adds external links to the system.

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,16 +1,19 @@
 import '../styles/nav-bar.scss';
+import _ from 'lodash';
 import routesStrings from '../../assets/strings/routes.json';
 import sharedStrings from '../../assets/strings/shared.json';
 
 class NavBarController {
   /** @ngInject */
-  constructor($log, $state, $mdSidenav) {
+  constructor($log, $state, $window, $mdSidenav) {
     this.log = $log;
     this.state = $state;
+    this.window = $window;
     this.mdSidenav = $mdSidenav;
     this.routes = routesStrings.routes;
     this.contactPhone = sharedStrings.contactPhone;
     this.contactEmail = sharedStrings.contactEmail;
+    this.systemLinks = sharedStrings.systemLinks;
   }
 
   goto(path) {
@@ -20,6 +23,11 @@ class NavBarController {
       }
       this.state.go(path);
     }
+  }
+
+  goToSystem(linkId) {
+    const url = _.find(this.systemLinks, {id: linkId}).link;
+    this.window.open(url, '_blank');
   }
 
   toggleSideNav() {
--- src/app/containers/Home.js
+++ src/app/containers/Home.js
@@ -1,13 +1,21 @@
 import '../styles/home.scss';
+import _ from 'lodash';
 import homeStrings from '../../assets/strings/home.json';
+import sharedStrings from '../../assets/strings/shared.json';
 
 class HomeController {
   /** @ngInject */
-  constructor($state) {
-    this.state = $state;
+  constructor($window) {
+    this.window = $window;
     this.heroSection = homeStrings.heroSection;
     this.values = homeStrings.values;
     this.areas = homeStrings.areas;
+    this.systemLinks = sharedStrings.systemLinks;
+  }
+
+  goToSystem(linkId) {
+    const url = _.find(this.systemLinks, {id: linkId}).link;
+    this.window.open(url, '_blank');
   }
 
 }
